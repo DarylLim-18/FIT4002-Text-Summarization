d4f5bd2fb37474e2ee40b94c931157b0b741ad27
DarylLim-18 <daryllim43@gmail.com>
2025-10-05 21:30:54 +0800

    SQL schema setup

--- backend/SQL/setup.sql
+++ backend/SQL/setup.sql
@@ -1,17 +1,12 @@
-CREATE DATABASE IF NOT EXISTS FIT4002;
-USE FIT4002;
-DROP TABLE IF EXISTS file;
+DROP TABLE files;
 
-CREATE TABLE file (
-    file_id INT AUTO_INCREMENT PRIMARY KEY,
-    file_name VARCHAR(255),
-    file_type ENUM('pdf', 'docx', 'txt') NOT NULL,
-    file_mime ENUM('application/pdf', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'text/plain') NOT NULL,
-    file_upload_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    file_size INT,
-    file_path VARCHAR(255),
-    file_description TEXT,
-    file_summary_text LONGTEXT
+CREATE TABLE files (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  file_name    VARCHAR(255) NOT NULL,
+  file_path    VARCHAR(255) NOT NULL,
+  file_size    BIGINT       NOT NULL,
+  file_type    VARCHAR(100)  NOT NULL,
+  file_summary TEXT         NOT NULL, 
+  file_content LONGTEXT NOT NULL,
+  upload_date  DATETIME     DEFAULT CURRENT_TIMESTAMP
 );
-
-
